52240cb8c1307e7a6519732f655cf738dcc9b0b9
Caio Atala <155749348+caioatala@users.noreply.github.com>
2024-03-22 18:45:21 +0000

    ...

---
+++ aula14/ambiente.js
@@ -0,0 +1,23 @@
+
+console.log('Vai comecar...')
+for(var c = 1; c <= 4; c++ ){
+    console.log(c)
+}
+console.log('FIM!')
+
+/*var c = 1 
+while ( c<= 5){
+    console.log ( c)
+    c++
+}
+*/
+
+/*
+var c = 1 
+do{
+    console.log ( c)
+    c++
+} while ( c<= 5)
+ */
+
+/* NOS TRES MODOS TEMOS O MESMO RESULTADO, SO MUDANDO A FORMA, UTILIZANDO O (WHILE / FOR / DO WHILE)  */
